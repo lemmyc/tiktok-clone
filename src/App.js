7b538486d8034552083817f95c71ce07c611c118
lemmyc <mrburgundy2911@gmail.com>
2022-05-26 23:31:07 +0700

    63: Layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 function App() {
     return (
         <Router>
             <div className="App">
                 <Routes>
                     {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            console.log(typeof route.layout);
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
                         const Page = route.component;
-                        return <Route key={index} path={route.path} element={<Page />} />;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
                     })}
                 </Routes>
             </div>
